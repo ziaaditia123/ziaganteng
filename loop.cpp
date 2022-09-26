67d9024fb4976ab5987be4246585f5f049734740
ziaaditia123 <113914410+ziaaditia123@users.noreply.github.com>
2022-09-26 17:54:35 +0700

    Add files via upload

---
+++ loop.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+
+using namespace std;
+
+int main () {
+    for (int x = 1; x <=1000;x++) {
+        cout << x << endl; }
+        cout << "zia aditia" <<endl;
+	
+}
+
---
+++ zia2.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+
+using namespace std;
+
+int main () {
+	int j;
+	    for(j=10 ; j>0 ;j--)
+		  
+		    printf("ziaganteng\n",j--);
+}
+
